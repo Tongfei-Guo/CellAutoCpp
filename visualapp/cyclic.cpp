c77a4594b36d512b7cf772123a1d81a3fbe1ac06
Xiaotian <h648194212@gmail.com>
2018-04-26 12:20:11 -0400

    move app src

---
+++ visualapp/cyclic.cpp
@@ -0,0 +1,77 @@
+//test purpose, don't delete until production
+#include <random>
+#include "Model.h"
+#include "Cell.h"
+#include "CAWorld.h"
+#include "CATypes.h"
+#include "CAFunctions.h"
+#include <chrono>
+#include <iostream>
+
+//render:
+#include <thread>
+#include <CArender.hpp>
+
+#include <iostream>
+
+
+int main()
+{
+
+    std::vector<bitcolor> palette = {
+    {255,0,0,255}, {255,96,0,255}, {255,191,0,255}, {223,255,0,255},
+    {128,255,0,255}, {32,255,0,255}, {0,255,64,255}, {0,255,159,255},
+    {0,255,255,255}, {0,159,255,255}, {0,64,255,255}, {32,0,255,255},
+    {127,0,255,255}, {223,0,255,255}, {255,0,191,255}, {255,0,96,255}
+    };
+    
+    CARender render(50,50,palette);
+    using namespace std::chrono_literals;
+
+    auto process = process_type([] (const grid_type &grid, Cell *self)
+    {
+        std::vector<Cell*> neighbors = get_neighbors(grid, self->x, self->y);
+        int next=((*grid[self->x][self->y])["state"]+rand()%2)%16;
+        bool changing=false;
+        for(auto n: neighbors)
+            if (n!=NULL)
+                changing=changing||((*n)["state"]==next);
+        if (changing)
+            (*grid[self->x][self->y])["state"]=next;
+    });
+    auto reset = reset_type();
+    auto init = init_type([](Cell *self)
+    {
+        (*self)["state"] = rand()%16;
+    });
+    auto getcolor = getcolor_type([](Cell *self)
+    {
+    	return (*self)["state"];
+    });
+
+    //CAWorld world1(Model(world_param_type(100, 50, 6), { grid_param_type("Wall", 100, process, reset, init) },0));
+    //CAWorld world2(Model(world_param_type(100, 50, 6), { grid_param_type("Wall", 100, process, reset, init) },1));
+    CAWorld world(Model(world_param_type(50, 50, 6), { grid_param_type("Cyclic", 100, process, reset, init) },1,getcolor));
+    //world.AddMeasure(new CADistributionMeasure());
+	auto start = std::chrono::high_resolution_clock::now();
+
+    for(int i = 0; i < 10000; i++)
+    {
+        world.forall_step(1);
+        auto bitmap = world.print_world();
+        if(!render.Renderworld(bitmap)) break;
+    }
+
+	
+    auto elapsed = std::chrono::high_resolution_clock::now() - start;
+    auto nanoseconds = std::chrono::duration_cast<std::chrono::nanoseconds>(elapsed).count();
+    std::cout << nanoseconds << "nanoseconds\n";
+    auto timestamp = world.get_timestamps();
+    world.print_test(timestamp, 0);
+    world.AddMeasureAndRun(new CADistributionMeasure("Final Dist"));
+
+    for(auto m: world.GetMeasures())
+        std::cout<<"========="<<m->GetName()<<"========="<<std::endl<<m->Str_All()<<std::endl;
+
+	return 0;
+}
---
+++ visualapp/watercave.cpp
@@ -0,0 +1,203 @@
+//test purpose, don't delete until production
+#include <random>
+#include "Model.h"
+#include "Cell.h"
+#include "CAWorld.h"
+#include "CATypes.h"
+#include "CAFunctions.h"
+#include <chrono>
+#include <iostream>
+#include <cmath>
+#include "CAMeasure.h"
+
+//render:
+#include <thread>
+#include <CArender.hpp>
+
+#include <iostream>
+
+//generate a cave filled with water
+//step 1: generate a vaccum cave
+//step 2: fil the cave with water
+int main()
+{
+    std::vector<bitcolor> palette = {
+    		{89, 125, 206, 0},         //0
+    		{89, 125, 206, 255/9},     //1
+                {89, 125, 206, 2*255/9},   //2
+                {89, 125, 206, 3*255/9},   //3
+                {89, 125, 206, 4*255/9},   //4
+                {89, 125, 206, 5*255/9},   //5
+                {89, 125, 206, 6*255/9},   //6
+                {89, 125, 206, 7*255/9},   //7
+                {89, 125, 206, 8*255/9},   //8 
+                {89, 125, 206, 9*255/9},   //9  
+                {255,255,255,255},         //10
+                {0,0,0,255}                //11
+    };
+
+    int gridsize = 100;
+    CARender render(gridsize,gridsize,palette);
+    using namespace std::chrono_literals;
+
+    //step 1. generate a vaccum cave
+    auto vaccumcave_process = process_type([] (const grid_type &grid, Cell *self)
+    {
+        std::vector<Cell*> neighbors = get_neighbors(grid, self->x, self->y);
+        int surrounding = countSurroundingCellsWithValue(neighbors, "wasOpen");
+        (*grid[self->x][self->y])["open"] = ((*grid[self->x][self->y])["wasOpen"] && surrounding >=4 || surrounding >= 6);
+    });
+    auto vaccumcave_reset = reset_type([](Cell *self)
+    {
+        (*self)["wasOpen"] = (*self)["open"];
+    });
+    auto vaccumcave_init = init_type([](Cell *self)
+    {
+        (*self)["open"] = (((double) rand() / (RAND_MAX)) > 0.4);
+    });
+
+    auto vaccum_getcolor = getcolor_type([](Cell *self)
+    {
+        if((*self)["open"] == 1)
+	    return 10;
+	else
+	    return 11;
+    });
+
+    CAWorld vaccumcave(Model(world_param_type(gridsize, gridsize, 6), { grid_param_type("Wall", 100, vaccumcave_process, vaccumcave_reset, vaccumcave_init) },0,vaccum_getcolor));
+    //vaccumcave.forall_step(10);
+
+
+    auto  bitmap = vaccumcave.print_world();
+    for(int i = 0; i < 10; i ++)
+    {
+        vaccumcave.forall_step(1);
+        bitmap = vaccumcave.print_world();
+        if(!render.Renderworld(bitmap)) break;
+    }
+    
+    std::cin.get();
+    //bitmap = vaccumcave.print_world();
+    //if(!render.Renderworld(bitmap)) std::cout<<"done"<<std::endl;//break;
+
+
+    auto gridref = gettypeind_type([](Cell *self)
+    {
+        if((*self)["open"] == 1)
+    	    return std::string("water");
+        else
+            return std::string("Wall"); });
+
+    auto cavemap = vaccumcave.getgridref(gridref);
+
+    //step 2.fill the cave with water
+    auto fullcave_processs = process_type([](const grid_type &grid, Cell *self)
+    {
+        ; //do nothing
+    });
+    auto fullcave_init = init_type([](Cell *self)
+    {
+        (*self)["open"] = 0; //the cave cell must be blocked
+    });
+
+    //water cell behave
+    //water cell has 9 water level. Higher level indicates more water in the cell.
+    auto water_processs = process_type([](const grid_type &grid, Cell *self)
+    {
+    	//auto coord = get_coord(grid, self);
+        unsigned x = self->x, y = self->y;
+        
+        std::vector<Cell*> neighbors = get_neighbors(grid, x, y);
+
+        int thislevel = (*self)["level"];
+        if(thislevel == 0)
+        	return;
+
+
+        // cell below me will take all it can
+        if(neighbors[CAWorld::BOTTOM] != nullptr &&
+        	thislevel!=0 &&
+		   neighbors[CAWorld::BOTTOM]->get_type()=="water" && (*neighbors[CAWorld::BOTTOM])["level"] < 9 )
+        {
+        	int amt = std::min(thislevel,9 - (*neighbors[CAWorld::BOTTOM])["level"]);
+            (*self)["level"] -= amt;
+            (*neighbors[CAWorld::BOTTOM])["level"] += amt;
+        	return ;
+        }
+
+        // bottom two corners take half of what I have
+        for(int i = 5; i < 8; i++)
+        {
+        	if( i!=CAWorld::BOTTOM &&
+        	    neighbors[i] != nullptr &&
+		    thislevel != 0 &&
+		    neighbors[i]->get_type()=="water" && (*neighbors[i])["level"] < 9)
+        	{
+                int amt = std::min(thislevel, (int)std::ceil( (9.0 - (*neighbors[i])["level"])/2.0 ) );
+                (*self)["level"] -= amt;
+                (*neighbors[i])["level"] += amt;
+                return;
+        	}
+        }
+
+        // sides take a third of what I have
+        for(int i = 3; i < 5; i++)
+        {
+        	if (neighbors[i] != nullptr &&
+        		neighbors[i]->get_type()=="water" && (*neighbors[i])["level"]  < thislevel)
+        	{
+        		int amt = std::min(thislevel, (int)std::ceil( (9.0 - (*neighbors[i])["level"])/3.0 ) );
+        		(*self)["level"] -= amt;
+        		(*neighbors[i])["level"] += amt;
+        		return ;
+        	}
+        }
+
+    });
+
+
+    auto water_init = init_type([](Cell *self)
+    {
+        (*self)["level"] = (int)std::floor( ((double) rand() / (RAND_MAX)) * 9 ); //the cave cell must be blocked
+        //std::cout<<"waterlevel:"<<(*self)["level"]<<std::endl;
+    });
+
+    auto fullcave_getcolor = getcolor_type([](Cell *self)
+    {
+        
+        if(self->get_type()=="Wall")
+        {  
+            return 11;
+        }
+        else
+        {
+          //std::cout<<"water level"<<(*self)["levl"]<<std::endl;
+          return (*self)["level"];
+        } 
+    });
+
+
+
+    Model watercavemodel(world_param_type(gridsize, gridsize, 6),
+    		             { grid_param_type("Wall", 20, fullcave_processs, {}, fullcave_init) ,
+    		               grid_param_type("water",80, water_processs, {}, water_init)},1,fullcave_getcolor);
+    CAWorld fullcave(watercavemodel);
+    fullcave.initgridfromgridref(cavemap);
+
+    fullcave.AddMeasure(new CADistributionMeasure());
+
+    //fullcave.forall_step(1);
+
+    bitmap = fullcave.print_world();
+    for(int i = 0; i < 10000; i ++)
+    {
+        fullcave.forall_step(1);
+        bitmap = fullcave.print_world();
+        //for (auto m: fullcave.GetMeasures())
+	//    std::cout<<m->Str_Current()<<std::endl;
+        if(!render.Renderworld(bitmap)) break;
+    }
+
+    std::cout<<"end of program"<<std::endl;
+	return 0;
+}
