527829048589a06e7b32e1f20f9ab5a7a3ed420a
Shoutian Hu <h648194212@gmail.com>
2018-04-26 17:37:05 -0400

    delete get_coord_error

--- CAException.h
+++ CAException.h
@@ -52,19 +52,6 @@ private:
 	std::string message;
 };
 
-class CAWorld_param_error : public std::exception
-{
-public:
-	CAWorld_param_error(const std::string &&c, const std::string &&p) : ctor(std::move(c)), param(p){}
-	virtual const char* what() const throw()
-	{
-		return (ctor + " constructor's argument : " + param + " cannot be 0").c_str();
-	}
-private:
-	std::string ctor;
-	std::string param;
-};
-
 class internal_error : public std::exception
 {
 public:
