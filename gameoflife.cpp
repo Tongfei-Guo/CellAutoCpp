c26f040c0ad4857f569d5fae8df89159cedb2801
cherudim9 <cherudim9@gmail.com>
2018-04-26 21:11:22 -0400

    Simplified gameoflife

--- gameoflife.cpp
+++ gameoflife.cpp
@@ -11,7 +11,7 @@ int main()
 {
     auto process = process_type([] (const grid_type &grid, Cell *self)
     {
-        std::vector<Cell*> neighbors = get_neighbors(grid, self->x, self->y);
+        auto neighbors = get_neighbors(grid, self->x, self->y);
         auto cnt=countSurroundingCellsWithValue(neighbors, "wasAlive");
         (*self)["alive"]=(cnt==3 || (cnt==2 && (*self)["alive"]));
     });
@@ -38,15 +38,9 @@ int main()
     };
     */
 
-    //CAWorld world1(Model(world_param_type(100, 50, 6), { grid_param_type("Wall", 100, process, reset, init) },0));
-    //CAWorld world2(Model(world_param_type(100, 50, 6), { grid_param_type("Wall", 100, process, reset, init) },1));
     CAWorld world(Model(world_param_type(96, 64, 6), { grid_param_type("living", 100, process, reset, init) },1, getcolor));
     //world.AddMeasure(new CADistributionMeasure());
-	auto start = std::chrono::high_resolution_clock::now();
 	world.forall_step(1000);
-    auto elapsed = std::chrono::high_resolution_clock::now() - start;
-    auto nanoseconds = std::chrono::duration_cast<std::chrono::nanoseconds>(elapsed).count();
-    std::cout << nanoseconds << "nanoseconds\n";
     auto timestamp = world.get_timestamps();
     world.print_test(timestamp, 0);
     world.AddMeasureAndRun(new CADistributionMeasure("Final Dist"));
