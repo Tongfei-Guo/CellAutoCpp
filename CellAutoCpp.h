817432e03cf1b1545b7d74ffefb3a38738e16b1c
glen <glenjoker@gmail.com>
2018-04-30 17:35:00 -0400

    add header wrapper

---
+++ CellAutoCpp.h
@@ -0,0 +1,13 @@
+#ifndef CELL_AUTO_CPP_H
+#define CELL_AUTO_CPP_H
+
+#include "CATypes.h"
+#include "CAException.h"
+#include "Cell.h"
+#include "CAWorld.h"
+#include "Model.h"
+#include "CAFunctions.h"
+#include "CAMeasure.h"
+#include "classicmodel.h"
+
+#endif
